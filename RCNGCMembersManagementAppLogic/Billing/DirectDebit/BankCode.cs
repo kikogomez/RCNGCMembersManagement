4a292c1dc24604bd3b3dca7f4909c55054fe5471
Francisco Gomez-Caldito <kikogomez@gmail.com>
2013-11-11 23:12:15 +0000

    BankCodes

---
+++ RCNGCMembersManagementAppLogic/Billing/DirectDebit/BankCode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace RCNGCMembersManagementAppLogic.Billing.DirectDebit
+{
+    [System.Serializable()]
+    public class BankCode
+    {
+        string localBankCode;
+        string name;
+        string bIC;
+
+        //Paramenterless constructor for serialization purposes
+        private BankCode() { }
+
+        public BankCode(string localBankCode, string name, string bIC)
+        {
+            this.localBankCode = localBankCode;
+            this.name = name;
+            this.bIC = bIC;
+        }
+
+        [XmlElement("LocalBankCode")]
+        public string LocalBankCode
+        {
+            get { return localBankCode; }
+            set { localBankCode = value; }
+        }
+
+        [XmlElement("BankName")]
+        public string BankName
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        [XmlElement("BIC")]
+        public string BankBIC
+        {
+            get { return bIC; }
+            set { bIC = value; }
+        }
+    }
+}
