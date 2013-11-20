ceb6dd52f6184790b01439d870e87c3596c555c3
Francisco Gomez-Caldito <kikogomez@gmail.com>
2013-11-20 22:26:22 +0000

    DirectDebitRemmetanceTests

---
+++ RCNGCMembersManagementAppLogic/DirectDebitRemmitance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCNGCMembersManagementAppLogic
+{
+    public class DirectDebitRemmitance
+    {
+        string remmitanceID;
+        DateTime creationDateTime;
+        int numberOfTransactions;
+        double controlSum;
+
+        public DirectDebitRemmitance(DateTime creationDate)
+        {
+        }
+
+        private void GenerateRemmitanceID()
+        {
+
+        }
+    }
+}
---
+++ RCNGCMembersManagementUnitTests/DirectDebitRemmitanceUnitTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RCNGCMembersManagementMocks;
+using RCNGCMembersManagementAppLogic;
+using RCNGCMembersManagementAppLogic.Billing;
+using RCNGCMembersManagementAppLogic.Billing.DirectDebit;
+using RCNGCMembersManagementAppLogic.MembersManaging;
+
+
+namespace RCNGCMembersManagementUnitTests
+{
+    [TestClass]
+    public class DirectDebitRemmitanceUnitTest
+    {
+        static Dictionary<string, ClubMember> clubMembers;
+        static Creditor creditor;
+        static CreditorAgent creditorAgent;
+        static InvoicesManager invoicesManager;
+
+        BillingDataManager billingDataManager;
+        List<Bill> unassignedBillsList;
+
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            BillingSequenceNumbersMock invoiceDataManagerMock = new BillingSequenceNumbersMock();
+            BillingDataManager.Instance.SetBillingSequenceNumberCollaborator(invoiceDataManagerMock);
+            BillingDataManager.Instance.InvoiceSequenceNumber=5000;
+            invoicesManager = new InvoicesManager();
+
+            clubMembers = new Dictionary<string, ClubMember>()
+            {
+                {"00001", new ClubMember("00001", "Francisco", "Gómez-Caldito", "Viseas")},
+                {"00002", new ClubMember("00002", "Pedro", "Pérez", "Gómez")}
+            };
+            creditor = new Creditor("G35008770", "Real Club Náutico de Gran Canaria");
+            creditorAgent = new CreditorAgent(new BankCode("2100", "CaixaBank","CAIXESBBXXX"));
+
+            var billsList = new[]
+            {
+                new {clubMemberID = "00001", Amount = 79, transactionDescription = "Cuota Social Octubre 2013" },
+                new {clubMemberID = "00002", Amount = 79, transactionDescription="Cuota Social Octubre 2013" },
+                new {clubMemberID = "00002", Amount = 79, transactionDescription="Cuota Social Noviembre 2013"}
+            };
+
+            foreach (var bill in billsList)
+            {
+                List<Transaction> transaction = new List<Transaction>()
+                {
+                    new Transaction(bill.transactionDescription,1,bill.Amount,new Tax("NoTAX",0),0)
+                };
+                ClubMember clubMember = clubMembers[bill.clubMemberID];
+                InvoiceCustomerData invoiceCustomerData = new InvoiceCustomerData(clubMember);
+                Invoice invoice = new Invoice(invoiceCustomerData, transaction, new DateTime(2013, 11, 11));
+                invoicesManager.AddInvoiceToClubMember(invoice, clubMember);
+            }
+        }
+
+        [TestMethod]
+        public void ADirectDebittRemmitanceInstanceIsCorrectlyCreated()
+        {
+            //DateTime creationDate =
+            //DirectDebitRemmitance directDebitRemmitance = new DirectDebitRemmitance(
+
+
+        }
+    }
+}
