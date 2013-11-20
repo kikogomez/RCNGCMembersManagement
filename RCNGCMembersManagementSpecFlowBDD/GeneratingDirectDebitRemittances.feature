15dd5b61a14d29809a78af68ea3b207026bc7cc2
Francisco Gomez-Caldito <kikogomez@gmail.com>
2013-11-20 19:57:50 +0000

    Generating Direct Debit Remmitances

---
+++ RCNGCMembersManagementSpecFlowBDD/GeneratingDirectDebitRemittances.feature
@@ -0,0 +1,26 @@
+﻿Feature: Generating Direct Debit Remmitances
+	In order to charge the bills to my club members
+	As an administrative assistant
+	I want to generate direct debit remitances to bank
+
+Background: 
+	
+	Given My Direct Debit Initiation Contract is
+	| NIF       | Name                              | BIC         | CreditorAgentName | LocalBankCode | CreditorBussinesCode | CreditorAccount          |
+	| G35008770 | Real Club Náutico de Gran Canaria | CAIXESBBXXX | CAIXABANK         | 2100          | 777                  | ES5621001111301111111111 |
+
+	Given These Club Members
+	| MemberID | Name      | FirstSurname  | SecondSurname | DirectDebitReferenceNumber | IBAN                     | BIC         |
+	| 00001    | Francisco | Gomez-Caldito | Viseas        | 1234                       | ES1801821234861234567890 | BBVAESMMXXX |
+	| 00002    | Pedro     | Perez         | Gomez         | 1235                       | ES0321001234561234567890 | CAIXESBBXXX |
+
+	Given These bills
+	| MemberID | TransactionConcept           | Amount |
+	| 00001    | Cuota Mensual Octubre 2013   | 79     |
+	| 00002    | Cuota Mensual Octubre 2013   | 79     |
+	| 00002    | Cuota Mensual Noviembre 2013 | 79     |
+
+Scenario: Create a new direct debit remmitance
+	Given I have a I have a direct debit initiation contract
+	When I generate a new direct debit remmitance
+	Then An empty direct debit remmitance is created
--- RCNGCMembersManagementSpecFlowBDD/GeneratingDirectDebitRemmitancesFeatureSteps.cs
+++ RCNGCMembersManagementSpecFlowBDD/GeneratingDirectDebitRemmitancesFeatureSteps.cs
@@ -1,25 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RCNGCMembersManagementAppLogic.MembersManaging;
+using RCNGCMembersManagementAppLogic.Billing.DirectDebit;
 
 namespace RCNGCMembersManagementSpecFlowBDD
 {
     [Binding, Scope(Feature = "Generating Direct Debit Remmitances")]
     public class GeneratingDirectDebitRemmitancesFeatureSteps
     {
+        [Given(@"My Direct Debit Initiation Contract is")]
+        public void GivenMyDirectDebitInitiationContractIs(Table contractTable)
+        {
+            Creditor creditor = new Creditor(contractTable.Rows[0]["NIF"], contractTable.Rows[0]["Name"]);
+            BankCode creditorAgentBankCode = new BankCode(
+                contractTable.Rows[0]["LocalBankCode"],
+                contractTable.Rows[0]["CreditorAgentName"],
+                contractTable.Rows[0]["BIC"]);
+            CreditorAgent creditorAgent = new CreditorAgent(creditorAgentBankCode);
+            BankAccount creditorAccount = new BankAccount(new InternationalAccountBankNumberIBAN(contractTable.Rows[0]["CreditorAccount"]));
+            string creditorBussinessCode = contractTable.Rows[0]["CreditorBussinesCode"];
+            DirectDebitInitiationContract directDebitContract = new DirectDebitInitiationContract(
+                creditorAccount,
+                creditor.NIF,
+                contractTable.Rows[0]["CreditorBussinesCode"],
+                creditorAgent);
+            ScenarioContext.Current.Add("Creditor", creditor);
+            ScenarioContext.Current.Add("CreditorAgent", creditorAgent);
+            ScenarioContext.Current.Add("DirectDebitContract", directDebitContract);
+        }
+        
+        [Given(@"These Club Members")]
+        public void GivenTheseClubMembers(Table table)
+        {
+            string pepe = "";
+        }
+        
+        [Given(@"These bills")]
+        public void GivenTheseBills(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
         [Given(@"I have a I have a direct debit initiation contract")]
         public void GivenIHaveAIHaveADirectDebitInitiationContract()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Given(@"I have direct debit remmitance")]
-        public void GivenIHaveDirectDebitRemmitance()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Given(@"I have a bill with a direct debit mandate asssociated")]
-        public void GivenIHaveABillWithADirectDebitMandateAsssociated()
         {
             ScenarioContext.Current.Pending();
         }
@@ -30,26 +55,8 @@ namespace RCNGCMembersManagementSpecFlowBDD
             ScenarioContext.Current.Pending();
         }
         
-        [When(@"I assign the bill to de direct debit remmitance")]
-        public void WhenIAssignTheBillToDeDirectDebitRemmitance()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
         [Then(@"An empty direct debit remmitance is created")]
         public void ThenAnEmptyDirectDebitRemmitanceIsCreated()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Then(@"The bill is marked as ""(.*)""")]
-        public void ThenTheBillIsMarkedAs(string p0)
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
-        [Then(@"The bill is added to the direct debit remmitance")]
-        public void ThenTheBillIsAddedToTheDirectDebitRemmitance()
         {
             ScenarioContext.Current.Pending();
         }
